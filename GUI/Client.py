fbfd70d262be0bd7c250919e75a5832088805b3c
achmads23 <achmads123@yahoo.com>
2015-03-07 14:29:19 +0700

    GUI Chat

--- GUI/Client.py
+++ GUI/Client.py
@@ -27,6 +27,25 @@ class Login(Frame):
                 self.Label_Sembunyi = Label(self, text="Username tidak tersedia")
                 self.Button_Submit = Button(self,text = "Log In", command=self.show_label)
                 self.Button_Submit.grid(padx=50,pady=5)
+
+class Chatting(Frame):
+        def __init__(self,master):
+                Frame.__init__(self,master)
+                self.create_widget()
+                
+        def create_widget(self):
+                self.grid()
+                self.Text_Chat = Text(self, bd=2, bg="white", height="20", width="42", font="Arial")
+                self.Text_Chat.config(state=DISABLED)
+                self.Scroll_Bar = Scrollbar(self, command=self.Text_Chat.yview, cursor="heart")
+                self.Text_Chat['yscrollcommand'] = self.Scroll_Bar.set
+                self.Send_Button = Button(self, font=30, text="Send", width="8", height=3, bd=0, bg="#FFBF00", activebackground="#FACC2E",command="")
+                self.Entry_Box = Text(self, bd=2, bg="white",width="35", height="3", font="Arial")
+
+                self.Scroll_Bar.grid(row=1,column=5,sticky='ns')
+                self.Text_Chat.grid(row=1,columnspan=5)
+                self.Entry_Box.grid(row=2,columnspan=3)
+                self.Send_Button.grid(row=2,column=4,columnspan=2)
         
 client = socket.socket()
 client.connect(('localhost', 7891))
