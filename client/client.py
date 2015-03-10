154ef5d9fb03a2c804789ce05bf54c555163560e
Dinar Winia Mahandhira <dinar.mahandhira@gmail.com>
2015-03-10 22:22:04 +0700

    update client, handle login failed

--- client/client.py
+++ client/client.py
@@ -1,28 +1,31 @@
 import socket
 import sys
-import shutil
-import select
+import threading
 
-host = 'localhost'
-port = 1212
+host = '10.151.43.223'
+port = 22001
 size = 1024
+login = 0
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host,port))
 
 sys.stdout.write('You are connected to server, please login to continue...\n')
 
 try:
-    while 1:
-        # baca input dan mengirim ketika ada input enter
-        line = sys.stdin.readline()
-        if line == '\n':
-            break
-        if line == 'Connected':
-            sname = raw_input("Enter Username: ")
-        print "You: " + line
-        s.send(line)
+    while login==0:
+        # masukin username
+        cname = raw_input("Username: ")
+        s.send('login:'+cname)
+        msg = s.recv(size)
+        auth = msg.split(':')[1]
+        if auth=='1':
+            login = 1
+        else:
+            print msg.split(':')[2]
+    
+    #bikin GUI list
+    
 
 except KeyboardInterrupt:
     s.close()
     sys.exit(0)
-        
