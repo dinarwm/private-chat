0718de9ac00e3cadec9b47fad294d726654eabb2
achmads23 <achmads123@yahoo.com>
2015-03-11 01:53:09 +0700

    Sudah bisa, tinggal ngatur chat sama thread sama select aja

--- client/client.py
+++ client/client.py
@@ -37,7 +37,7 @@ class ListOnline(Frame):
         def create_widget(self):
                 self.Label_Password = Label(self,text = "Online Users")
                 self.Label_Password.pack()
-                self.var = ""
+                self.var = IntVar()
                 msg = s.recv(size)
                 print msg
                 words = msg.split(':')
@@ -56,9 +56,37 @@ class ListOnline(Frame):
                 self.Button_Submit.pack(padx=50,pady=5)
 
         def sel(self):
-                selection = "You selected the option " + str(self.var)
-                self.label.config(text = selection)
-                print str(self.var.get())
+                print(str(self.var.get()))
+                Chat(str(self.var.get()))
+                self.master.destroy()
+                
+class Chatting(Frame):
+        global urutan
+        urutan = 1
+        global kata
+        kata = ""
+        def __init__(self,master,string):
+                Frame.__init__(self,master)
+                self.grid()
+                self.create_widget()
+                kata = string
+                print(string)
+                
+        def create_widget(self):
+                self.Text_Chat = Listbox(self, bd=2, bg="white", height="20", width="42", font="Arial")
+                self.Scroll_Bar = Scrollbar(self, command=self.Text_Chat.yview, cursor="heart")
+                self.Text_Chat['yscrollcommand'] = self.Scroll_Bar.set
+                self.Send_Button = Button(self, font=30, text="Send", width="8", height=3, bd=0, bg="#FFBF00", activebackground="#FACC2E",command=self.cetak)
+                self.Entry_Box = Text(self, bd=2, bg="white",width="35", height="3", font="Arial")
+
+                self.Scroll_Bar.grid(row=1,column=5,sticky='ns')
+                self.Text_Chat.grid(row=1,columnspan=5)
+                self.Entry_Box.grid(row=2,columnspan=3)
+                self.Send_Button.grid(row=2,column=4,columnspan=2)
+
+        def cetak(self):
+                abc = self.Entry_Box.get("1.0",END)
+                self.Text_Chat.insert(END,abc)
 
 def Log():
         root = Tk()
@@ -71,7 +99,13 @@ def List():
         root.title("List Online Users")
         app = ListOnline(root)
         root.mainloop()
-        
+
+def Chat(string):
+        root = Tk()
+        root.title("Chatting Users")
+        app = Chatting(root,string)
+        root.mainloop()
+
 host = '10.151.43.223'
 port = 22001
 size = 1024
