6d28d310f4cdbf13d8edda31610fac1a2a7f3838
Risyanggi Azmi Faizin <switgarlik@gmail.com>
2015-03-10 22:33:09 +0000

    udah protocol, kadang segfault gatau knp

--- server/server.c
+++ server/server.c
@@ -17,13 +17,13 @@
 #define PORT 22001
 #define MAX_QUEUES 10
 #define MAX_THREADS 256
-#define MAX_NAME_LENGTH 16
+#define MAX_NAME_LENGTH 10
 #define MAX_BUFFER 100
 
 typedef struct node_t {
 	int id;
 	int socketfd;
-	char name[MAX_NAME_LENGTH];
+	char name[MAX_NAME_LENGTH + 1];
 	struct node_t *next;
 } node_t;
 
@@ -47,10 +47,6 @@ void initialize() {
  *
  * @return node*|NULL
  */
-node_t *find(int id) {
-	return _find(list, id);
-}
-
 node_t *_find(node_t *node, int id) {
 	if (node == NULL) {
 		return NULL;
@@ -59,6 +55,33 @@ node_t *_find(node_t *node, int id) {
 		return node;
 	}
 	return _find(node->next, id);
+}
+
+node_t *find(int id) {
+	return _find(list, id);
+}
+
+/**
+ * Get node from the list by given name. If it 
+ * doesn't exist, return NULL.
+ *
+ * @param char *name
+ *
+ * @return node*|NULL
+ */
+
+node_t *_find_by_name(node_t *node, char *name) {
+	if (node == NULL) {
+		return NULL;
+	}
+	if (!strcmp(node->name, name)) {
+		return node;
+	}
+	return _find_by_name(node->next, name);
+}
+
+node_t *find_by_name(char *name) {
+	return _find_by_name(list, name);
 }
 
 /**
@@ -83,17 +106,33 @@ node_t *new_node(int id, int socketfd, char *name) {
 }
 
 /**
+ * Print and return list size.
+ *
+ * @return int size
+ */
+int list_size() {
+	node_t *temp = list;
+	int i;
+	for (i=0; temp != NULL; i++) {
+		temp = temp->next;
+	}
+	printf("Current list size is %d.\n", i);
+	return i;
+}
+
+/**
  * Add given node to the list.
  *
  * @param node*
  */
 void push(node_t *node) {
 	if (list == NULL) {
-		list = x;
+		list = node;
 	} else {
 		node->next = list;
 		list = node;
 	}
+	list_size();
 }
 
 /**
@@ -105,32 +144,26 @@ void push(node_t *node) {
  * @return int 1|0
  */
 int pop(int id) {
-	temp = list;
+	if (list == NULL) return 0;
+
+	node_t *temp = list;
 	if (list->id == id) {
 		list = list->next;
 		free(temp);
+		list_size();
 		return 1;
 	}
-	while (temp->next != null) {
+	while (temp->next != NULL) {
 		if (temp->next->id == id) {
 			temp->next = temp->next->next;
 			free(temp->next);
+			list_size();
 			return 1;
 		}
 		temp = temp->next;
 	}
+	list_size();
 	return 0;
-}
-
-/**
- * Handles new client.
- *
- * @param int id generated from main.
- * @param int socket descriptor of new client.
- */
-void handle_accept(int id, int clientsock) {
-	node_t *node = new_node(id, clientsock, "undefined");
-	pthread_create(&threads[id], NULL, client_thread, (void *)node);
 }
 
 /**
@@ -179,18 +212,87 @@ void broadcast(char *msg) {
 /**
  * Create a userlist in a string format:
  *   userlist:<id>#<name>:<id>#<name>\r\n
- *
- * @return char *userlist
+ * Then broadcast to all connected client.
  */
-char *users() {
+void users() {
 	char ans[1024];
-	strcpy(ans, "users");
+	sprintf(ans, "users");
 	node_t *temp = list;
 	while (temp != NULL) {
 		sprintf(ans, "%s:%d#%s", ans, temp->id, temp->name);
 		temp = temp->next;
 	}
-	return ans;
+	sprintf(ans, "%s\r\n", ans);
+	broadcast(ans);
+
+	return;
+}
+
+/**
+ * Do login. If success, then assign username to the client
+ * node and return the client node. Otherwise return NULL.
+ *
+ * @param node_t client.
+ *
+ * @return node_t|NULL client.
+ */
+node_t *login(node_t *client) {
+	char buffer[MAX_BUFFER];
+	bzero(buffer, MAX_BUFFER);
+	int socketfd = client->socketfd;
+
+	my_read(socketfd, buffer, MAX_BUFFER);
+
+	char *ptr;
+
+	ptr = strtok(buffer, ":\r\n");
+
+	if (strcmp(ptr, "login")) {
+		printf(" (%d) Wrong login format: '%s'.\n", client->id, ptr);
+		sprintf(buffer, "auth:0:Wrong login format.\r\n");
+		my_write(socketfd, buffer, strlen(buffer)+1);
+		return NULL;
+	}
+
+	ptr = strtok(NULL, ":\r\n");
+	printf(" (%d) Name: %s\n", client->id, ptr);
+
+	if (strlen(ptr) > MAX_NAME_LENGTH) {
+		printf(" (%d) Name is too long: '%s'.\n", client->id, ptr);
+		sprintf(buffer, "auth:0:Name length more than %d.\r\n", MAX_NAME_LENGTH);
+		my_write(socketfd, buffer, strlen(buffer)+1);
+		return NULL;
+	} else if (find_by_name(ptr) != NULL) {
+		printf(" (%d) Name alredy used: '%s'.\n", client->id, ptr);
+		sprintf(buffer, "auth:0:Name alredy used.\r\n");
+		my_write(socketfd, buffer, strlen(buffer)+1);
+		return NULL;
+	}
+
+	strcpy(client->name, ptr);
+	sprintf(buffer, "auth:1:Login success.\r\n");
+	my_write(socketfd, buffer, strlen(buffer)+1);
+
+	return client;
+}
+
+/**
+ * Do logout. Close the socket descriptor. Broadcast to all
+ * online users if someone is logout. Finally pop the node_t.
+ *
+ * @param node_t *client.
+ */
+void logout(node_t *client) {
+	char buffer[16];
+	close(client->socketfd);
+	printf(" (%d) Close client with socket descriptor %d.\n", client->id, client->socketfd);
+	
+	sprintf(buffer, "off:%d\r\n", client->id);
+	broadcast(buffer);
+
+	int id = client->id;
+	int pop_return = pop(client->id);
+	printf(" (%d) Pop returns %d.\n", id, pop_return);
 }
 
 /**
@@ -198,15 +300,29 @@ char *users() {
  * is attached in the message. First, convert the message
  *   from : <recipient_id>:<message>\r\n
  *   to   : rcv:<sender_id>:<message>\r\n
+ * but if the recipient isn't on the list, send to sender
+ *   off:<recipient_id>\r\n
  *
  * @param node_t *sender node
  * @param char *msg sent to server
  */
 void deliver(node_t *sender, char *msg) {
-	char temp[strlen(msg) - 1];
-	int sender_id;
-	sscanf(msg, "%d:%s", &sender_id, temp);
+	char temp[strlen(msg) + 25];
+	char buffer[100];
 
+	msg = strtok(NULL, ":\r\n");
+	int recipient_id = atoi(msg);
+
+	node_t *recipient = find(recipient_id);
+	if (recipient == NULL) {
+		printf(" (%d) User with %d is offline.\n", sender->id, recipient_id);
+		sprintf(buffer, "off:%d\r\n", recipient_id);
+		my_write(sender->socketfd, buffer, sizeof(buffer) + 1);
+	} else {
+		msg = strtok(NULL, "\r\n");
+		sprintf(temp, "rcv:%d:%s\r\n", sender->id, msg);
+		printf(" (%d) Send: rcv:%d:%s\n", sender->id, sender->id, msg);
+	}
 }
 
 /**
@@ -218,32 +334,47 @@ void *client_thread(void *arg) {
 	node_t *data;
 	int id;
 	int socketfd;
-	char name[MAX_NAME_LENGTH];
+	char name[MAX_NAME_LENGTH +1];
 
 	data = (node_t *)arg;
 	id = data->id;
 	socketfd = data->socketfd;
-	strcpy(name, data->name);
 
-	printf(" (%d) Entering thread.\n", id, socketfd);
+	printf(" (%d) Entering thread.\n", id);
 
-	// harusnya disini dimintain nama client nya
+	while (login(data) == NULL) {
+		printf(" (%d) Login failed.\n", id);
+	}
+
+	printf(" (%d) Login success.\n", id);
+	push(data);
+	users();
 
 	char buffer[MAX_BUFFER];
 	int ln = 1;
 	while (ln > 0) {
 		bzero(buffer, MAX_BUFFER);
 		ln = my_read(socketfd, buffer, MAX_BUFFER);
-		printf(" (%d) Recv: \"%s\"\n", id, buffer);
+		if (ln <= 0) break;
 
-		// harusnya disini ada handle protocol.
+		char *ptr;
+		ptr = strtok(buffer, "\r\n");
+		printf(" (%d) Recv: \"%s\"\n", id, ptr);
 
-		my_write(socketfd, buffer, strlen(buffer)+1);
-		printf(" (%d) Send: \"%s\"\n", id, buffer);
+		ptr = strtok(NULL, ":");
+
+		if (!strcmp(ptr, "quit")) {
+			logout(data);
+		} else if (!strcmp(ptr, "send")) {
+			deliver(data, ptr);
+		} else {
+			sprintf(buffer, "error:Unknown command: '%s'.\r\n", ptr);
+			my_write(socketfd, buffer, strlen(buffer)+1);
+			printf(" (%d) Send: Unknown command\n", id);
+		}
+
 	}
-	pop(id);
-	close(socketfd);
-	printf(" (%d) Close client with socket descriptor %d\n", id, socketfd);
+	logout(data);
 
 	return NULL;
 }
@@ -252,6 +383,12 @@ void *client_thread(void *arg) {
  * Main program.
  */
 int main(int argc, char const *argv[]) {
+	close(4);
+	close(5);
+	close(6);
+	close(7);
+	close(8);
+	close(9);
 	struct sockaddr_in servaddr;
 
 	int servsock = socket(AF_INET, SOCK_STREAM, 0);
@@ -274,7 +411,8 @@ int main(int argc, char const *argv[]) {
 	for (id = 1; isLoop; id++) {
 		int clientsock = accept(servsock, (struct sockaddr*) NULL, NULL);
 		printf(" (%d) Accept client with socket descriptor %d\n", id, clientsock);
-		handle_accept(id, clientsock);
+		node_t *node = new_node(id, clientsock, "undefined");
+		pthread_create(&threads[id], NULL, client_thread, (void *)node);
 	}
 
 	printf("Server is closing.");
