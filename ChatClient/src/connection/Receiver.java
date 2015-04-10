8189a061ab7a073d628b46c9083acd354fba0cb1
achmads23 <achmads123@yahoo.com>
2015-04-10 15:26:23 +0700

    Receiver

---
+++ ChatClient/src/connection/Receiver.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection;
+
+import forms.FormChat;
+import forms.FormOnline;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import main.ChatClient;
+
+/**
+ *
+ * @author achmads23
+ */
+public class Receiver implements Runnable {
+    
+    private Thread thread;
+    private final FormOnline formonline;
+    private final Socket conn;
+
+    public Receiver(FormOnline online) {
+        conn = ChatClient.conn;
+        this.formonline = online;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                DataInputStream dis;
+                dis = new DataInputStream(conn.getInputStream());
+                String string = dis.readLine().trim();
+                System.out.println("server: (" + string + ")");
+                String[] parts = string.split(":", 2);
+                String protocol = parts[0];
+                String msg = parts[1];
+                if ("users".equals(protocol)) {
+                    System.out.println("masuk protocol users");
+                    formonline.userUpdate(msg);
+                } else if ("rcv".equals(protocol)){
+                    parts = msg.split(":", 2);
+                    String id = parts[0];
+                    String nama = formonline.getNama(id);
+                    FormChat form = ChatList.OpenForm(nama,id);
+                    form.chatUpdate(nama + "\t:" + parts[1] );
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(Receiver.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void start() {
+        thread = new Thread(this);
+        thread.start();
+    }
+
+}
